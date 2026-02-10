229588b5b1059743baf29fe6743faac7e3ebe64b
zm <zm@localhost>
2026-02-10 15:24:14 +0300

    net indicator fix

--- tmux_network_bw.sh
+++ tmux_network_bw.sh
@@ -2,7 +2,7 @@
 
 INTERVAL="1" # update interval in seconds
 
-network_name=$(tmux show-option -gqv "@network-bandwidth-interface")
+network_name=$(ip r | grep default | head -n1 | awk '{print $5}')
 
 main() {
     while true; do
